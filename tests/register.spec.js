3cb5f8995fb4a27143fbdc854055253a62d3f054
Bruno Figueiredo <brunonf15@gmail.com>
2026-05-22 20:29:34 +0100

    Add comment in register user test
    Added a comment for clarity in the test case.

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -5,6 +5,7 @@ import RegisterPage from '../pom/RegisterPage';
 test('Register a user', async ({ page }) => {
 
   const registerPage = new RegisterPage(page);
+  //comment
   await page.goto('https://automationexercise.com/login');
 
   
